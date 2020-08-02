0b66c699b5eb892440162da9c17bc1bb43913df1
Nguyen Duc Anh <anhnicky66@gmail.com>
2020-08-02 20:48:23 +0700

    deploy

---
+++ precache-manifest.7c1d0c26e85873456ee388253cd0c7bc.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48663aa999755ba1d159",
+    "url": "/css/app.2a502a36.css"
+  },
+  {
+    "revision": "59efb0687fe8db325a09",
+    "url": "/css/blogs.f31d0c35.css"
+  },
+  {
+    "revision": "5d186cc10d236105114d",
+    "url": "/css/chunk-vendors.5ff60f72.css"
+  },
+  {
+    "revision": "8b59e1f77c496eea4dfe",
+    "url": "/css/gafu.41e6a334.css"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/fonts/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/fonts/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/fonts/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/fonts/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/fonts/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/fonts/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/fonts/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/fonts/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/fonts/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/fonts/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/fonts/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/fonts/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/img/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/img/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/img/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "d4ad88aa1fd0df21c38183850d3261a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48663aa999755ba1d159",
+    "url": "/js/app.d823a6cc.js"
+  },
+  {
+    "revision": "59efb0687fe8db325a09",
+    "url": "/js/blogs.17135dd4.js"
+  },
+  {
+    "revision": "c90e1785b179ffc249e6",
+    "url": "/js/blogs~gafu.55b16d39.js"
+  },
+  {
+    "revision": "5d186cc10d236105114d",
+    "url": "/js/chunk-vendors.df345ec6.js"
+  },
+  {
+    "revision": "8b59e1f77c496eea4dfe",
+    "url": "/js/gafu.720e7c5b.js"
+  },
+  {
+    "revision": "d4d49ed5611bff99acceb8e1b2ad8a5f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cbdbebdfdf870c4df63320198a034c2f",
+    "url": "/svg/anh-nguyen.svg"
+  },
+  {
+    "revision": "18f7454378ae2802384b5df08e8a1538",
+    "url": "/svg/chrome.svg"
+  },
+  {
+    "revision": "0bb38f291554b3642e0917fcda0227cb",
+    "url": "/svg/frontend-dev.svg"
+  },
+  {
+    "revision": "d79a1432a655eaa9753f8807c5ee775d",
+    "url": "/svg/vuejs.svg"
+  }
+]);
